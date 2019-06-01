67beaf50e5f6a3e683c64daebe2cb7a61bcabe00
Artem <inste@icloud.com>
2019-06-01 21:31:40 +0300

    First  commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedGradients
+//
+//  Created by Артём Кармазь on 6/1/19.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
