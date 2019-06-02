fb33a721af9ba7fb95aa917bc621cb60e909769a
Artem <inste@icloud.com>
2019-06-02 10:47:46 +0300

    Fifth commit

--- ViewController.swift
+++ ViewController.swift
@@ -11,18 +11,27 @@ import UIKit
 class ViewController: UIViewController {
     
     private func createGradientLayer() {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
-        gradientLayer.frame = CGRect(origin: .zero, size: viewSize)
-        view.layer.addSublayer(gradientLayer)
+        
+//        let animatedGradient = AnimatedGradientView(frame: view.bounds)
+//        animatedGradient.direction = .up
+//        animatedGradient.animationValues = [(colors: ["#2BC0E4", "#EAECC6"], .up, .axial),
+//                                            (colors: ["#833ab4", "#fd1d1d", "#fcb045"], .right, .axial),
+//                                            (colors: ["#003973", "#E5E5BE"], .down, .axial),
+//                                            (colors: ["#1E9600", "#FFF200", "#FF0000"], .left, .axial)]
+//        view.addSubview(animatedGradient)
+//        
+//        //        let gradientLayer = CAGradientLayer()
+//        //        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+//        //        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+//        //        gradientLayer.frame = CGRect(origin: .zero, size: view!.bounds)
+        //        view.layer.addSublayer(gradientLayer)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        createGradientLayer()
     }
-
-
+    
+    
 }
 
